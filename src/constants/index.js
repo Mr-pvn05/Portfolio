01ed7f9c909cbe69a74c6ddbdc76d6d8dbbed78f
Praveen Singh <pvnsingh05@gmail.com>
2024-02-10 21:23:01 +0530

    Path fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,20 +1,20 @@
 export const projects = [
     {
-        imgURL: "/public/images/passwordGenerator.png",
+        imgURL: "/images/passwordGenerator.png",
         title: "Password Generator",
         intro: "Designed and built a password generator app using react hooks useState, useEffect, useCallback and useRef",
         liveDemo: "https://password-generator-gamma-teal.vercel.app/",
         sourceCode: "https://github.com/Mr-pvn05/Password-Generator"
     },
     {
-        imgURL: "/public/images/todo.png",
+        imgURL: "/images/todo.png",
         title: "Todo app",
         intro: "Crafted a dynamic React todo app using Context API, local storage; enabling edit, mark-as-done, and delete features seamlessly.",
         liveDemo: "https://todo-sepia-three.vercel.app/",
         sourceCode: "https://github.com/Mr-pvn05/Todo"
     },
     {
-        imgURL: "/public/images/nike.png",
+        imgURL: "/images/nike.png",
         title: "Nike Clone",
         intro: "Designed nike clone website using react and the tailwind Css.",
         liveDemo: "https://nike-jc33.vercel.app/",
